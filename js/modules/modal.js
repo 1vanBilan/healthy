c1453863e4c78e2a8a93125f28cdd1b79c7833a1
Ivan Bilan <bilan.vanya20044@gmail.com>
2022-08-16 19:45:11 +0300

    refreshed deadline for project

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -10,7 +10,6 @@ function openModal(modalSelector, modalTimerId) {
     modal.classList.add('show');
     modal.classList.remove('hide');
     document.body.style.overflow = 'hidden';
-    console.log(modalTimerId);
     if(modalTimerId){
         clearInterval(modalTimerId);
     }
